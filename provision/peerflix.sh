a9c897082387e92bcb46ef8bc061a2060f0255e5
Rick Wieman <git@rickw.nl>
2015-01-07 00:33:53 +0100

    Bugfixes for peerflix provision (PL)

--- provision/peerflix.sh
+++ provision/peerflix.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
+# This file currently only works for Fedora Core 8.
+
 # Let's begin with our dependencies =D
-sudo yum install -y git openssl-devel gcc gcc-c++ bzip2-devel
+sudo yum install -y git openssl-devel gcc gcc-c++ bzip2-devel automake
 
 # Now, let's update to Python 2.6 (from source)
 wget https://www.python.org/ftp/python/2.6.9/Python-2.6.9.tgz
@@ -15,6 +17,7 @@ sudo make install
 cd ..
 git clone --depth 1 git://github.com/joyent/node.git
 cd node
+git fetch
 git checkout v0.10.35
 ./configure
 make
